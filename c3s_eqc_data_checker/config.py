27c4ac68a0d2bf608e15f99a01e15e15593b97da
Mattia Almansi <m.almansi@bopen.eu>
2022-09-28 13:25:19 +0200

    move ext in config

--- c3s_eqc_data_checker/config.py
+++ c3s_eqc_data_checker/config.py
@@ -1,2 +1,24 @@
+# Copyright 2022, European Union.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+GRIB_EXTENSIONS = (
+    ".grib",
+    ".grb",
+    ".grb1",
+    ".grb2",
+)
 MIN_GRIB_EDITION = 2
+
+NETCDF_EXTENSIONS = (".nc",)
 MIN_NETCDF_EDITION = 4
--- c3s_eqc_data_checker/file_format.py
+++ c3s_eqc_data_checker/file_format.py
@@ -1,3 +1,16 @@
+# Copyright 2022, European Union.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import dataclasses
 import functools
 import mimetypes
@@ -15,8 +28,9 @@ except ImportError:
     HAS_DASK = False
 
 # Add netcdf and grib to mimetypes
-mimetypes.add_type("application/netcdf", ".nc", strict=True)
-for ext in (".grib", ".grb", ".grb1", ".grb2"):
+for ext in config.NETCDF_EXTENSIONS:
+    mimetypes.add_type("application/netcdf", ext, strict=True)
+for ext in config.GRIB_EXTENSIONS:
     mimetypes.add_type("application/x-grib", ext, strict=False)
 del ext
 
